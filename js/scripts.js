bccce43755de6606b048cc4721f9c23d40a04e4c
wkwyatt <wkwyatt1@gmail.com>
2015-11-03 14:24:48 -0500

    JS setup for movie category search

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,116 @@
-
-
-var apiKey = 'c95668aaeef845acc0e59e71d260f85c';
+$(document).ready(function(){
+	var searchedMovie ="";
+	var searchConfig="";
+	var searchOption="";
+	$('#movie-search-form').submit(function(){
+		event.preventDefault();
+		var searchURL = 'https://api.themoviedb.org/3/search/'
+		searchedMovie = $('#movieInput').val();
+	
+		searchOption = $('#search-options').val();
+		switch(searchOption){
+			case 'Movie':
+				searchConfig ='https://api.themoviedb.org/3/search/movie?query='+searchedMovie+'&api_key='+apiKey;
+				
+				$.getJSON(searchConfig, function(data){
+					console.log(data);
+					var html = "";
+					var x = 0;
+					movieArray = data.results;
+					$('#now-playing').html("<h2>Search Results</h2>");
+					$('#now-playing-wrapper').html("");
+					for(i=0; i<movieArray.length; i++){
+						x++;
+						var isAdult = movieArray[i].adult;
+						var backdrop_path = movieArray[i].backdrop_path;
+						var genre_ids = movieArray[i].genre_ids;
+						var movieId = movieArray[i].id;
+						var title = movieArray[i].title;
+						var overview = movieArray[i].overview;
+						var popularity = movieArray[i].popularity;
+						var posterPath = movieArray[i].poster_path;
+						var profilePath =movieArray[i].profile_Path;
+						var releaseDate = movieArray[i].release_date;
+						var voteAverage = movieArray[i].vote_average;
+						var voteCount = movieArray[i].vote_count;	
+						if(i==0){
+							html += '<div class="movie-row">';
+						}
+						if(x==5){
+							html += '</div>';
+							html += '<div class="movie-row">';
+							x=1;
+						}
+						html += '<div class="now-playing-movie">';
+						html += '<img title="'+overview+'" alt="'+title+'" src="'+basePath+'w300'+posterPath+'">';
+						html += '</div>';
+						
+						if(i == (movieArray.length-1)){
+							html += '</html>';
+							$(html).appendTo('#now-playing-wrapper');
+						}
+					}
+				});
+			break;
+			case 'Actor/Actress':
+				searchConfig ='https://api.themoviedb.org/3/search/person?query='+searchedMovie+'&api_key='+apiKey;
+				console.log(searchConfig);
+				$.getJSON(searchConfig, function(data){
+					console.log(data);
+					var html = "";
+					var x = 0;
+					movieArray = data.results;
+					$('#now-playing').html("<h2>Search Results</h2>");
+					$('#now-playing-wrapper').html("");
+					for(i=0; i<movieArray.length; i++){
+						x++;
+						var name= movieArray[i].name;
+						var knownFor = movieArray[i].known_for[0].original_title;
+						var backdropPath = movieArray[i].known_for[0].backdrop_path;
+						console.log(backdropPath);
+						var genre_ids = movieArray[i].genre_ids;
+						var movieId = movieArray[i].id;
+						var title = movieArray[i].title;
+						var overview = movieArray[i].overview;
+						var popularity = movieArray[i].popularity;
+						var posterPath = movieArray[i].poster_path;
+						var profilePath =movieArray[i].profile_path;
+						var releaseDate = movieArray[i].release_date;
+						var voteAverage = movieArray[i].vote_average;
+						var voteCount = movieArray[i].vote_count;	
+						if(i==0){
+							html += '<div class="movie-row">';
+						}
+						if(x==5){
+							html += '</div>';
+							html += '<div class="movie-row">';
+							x=1;
+						}
+						html += '<div class="now-playing-movie">';
+						html += '<img title="'+overview+'" alt="'+title+'" src="'+basePath+'w300'+profilePath+'"><p>'+name+'</p><p>Known for: '+knownFor+'</p>';
+						html += '<img src='+basePath+'w300'+backdropPath+'>';
+						html += '</div>';
+						
+						if(i == (movieArray.length-1)){
+							html += '</html>';
+							$(html).appendTo('#now-playing-wrapper');
+						}
+					}
+				});
+				break;
+			}
+	});
+console.log("The value of searchedMovie is " +searchedMovie);
+});
+var apiKey = "64bfb75ab93d0abeedff735b0bdbb192";
 var basePath = '';
 var sizeOptions = '';
 var logo_sizes = '';	
 var poster_sizes = '';
 var profile_sizes = '';
-
-
-$(document).ready(function() {
-	$('#movie-search-form').submit(function() {
-		event.preventDefault();
-		var thisSearch = $('#movieInput').val();
-		var searchMovie = 'https://api.themoviedb.org/3/search/movie?query='+ thisSearch +'&api_key='+apiKey;
-		$.getJSON(searchMovie, function(searchData) {
-			console.log(searchData);
-			var html = "";
-			var x = 0;
-			var searchMovieArray = searchData.results;
-
-			for(i=0; i<searchMovieArray.length; i++){
-				x++;
-				var isAdult = searchMovieArray[i].adult;
-				var backdrop_path = searchMovieArray[i].backdrop_path;
-				var genre_ids = searchMovieArray[i].genre_ids;
-				var movieId = searchMovieArray[i].id;
-				var title = searchMovieArray[i].title;
-				var overview = searchMovieArray[i].overview;
-				var popularity = searchMovieArray[i].popularity;
-				var posterPath = searchMovieArray[i].poster_path;
-				var releaseDate = searchMovieArray[i].release_date;
-				var voteAverage = searchMovieArray[i].vote_average;
-				var voteCount = searchMovieArray[i].vote_count;	
-
-				if(i==0){
-					html += '<div class="movie-row">';
-				}
-
-				if(x==5){
-					html += '</div>';
-					html += '<div class="movie-row">';
-					x=1;
-				}
-				html += '<div class="now-playing-movie">';
-				html += '<img title="'+overview+'" alt="'+title+'" src="'+basePath+'w300'+posterPath+'">';
-				html += '</div>';
-				$('#now-playing-wrapper').html(html);
-				if(i == (searchMovieArray.length-1)){
-					html += '</html>';
-					// $(html).appendTo('#now-playing-wrapper');
-				}
-			}
-			console.log(html);
-		});	
-	})
-	
-});
-
-
 var siteConfig = 'https://api.themoviedb.org/3/configuration?api_key='+apiKey;
-
 $.getJSON(siteConfig, function(data){
-	console.log(data);
+	// console.log(data);
 	basePath = data.images.base_url;
 	sizeOptions = data.images.poster_sizes;
 	//0: "w300" 1: "w780" 2: "w1280" 3: "original"
@@ -69,11 +118,9 @@ $.getJSON(siteConfig, function(data){
 	logo_sizes = logo_sizes['original'];
 	profileSizes = profile_sizes['original'];
 });
-
 var nowPlaying = 'http://api.themoviedb.org/3/movie/now_playing?api_key='+apiKey;
-
 $.getJSON(nowPlaying, function(data){
-	console.log(data);
+	// console.log(data);
 	var html = "";
 	var x = 0;
 	movieArray = data.results;
@@ -92,11 +139,9 @@ $.getJSON(nowPlaying, function(data){
 		var releaseDate = movieArray[i].release_date;
 		var voteAverage = movieArray[i].vote_average;
 		var voteCount = movieArray[i].vote_count;	
-
 		if(i==0){
 			html += '<div class="movie-row">';
 		}
-
 		if(x==5){
 			html += '</div>';
 			html += '<div class="movie-row">';
@@ -112,7 +157,3 @@ $.getJSON(nowPlaying, function(data){
 		}
 	}
 }); // End get json - popular movies
-
-
-
-
