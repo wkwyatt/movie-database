8d3f0f9c358f251135b0c485291cd764121cce3a
wkwyatt <wkwyatt1@gmail.com>
2015-11-03 10:45:01 -0500

    Added search method to find movies within database

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,62 @@
-var apiKey = 'YOURAPIKEYHERE';
+
+
+var apiKey = 'c95668aaeef845acc0e59e71d260f85c';
 var basePath = '';
 var sizeOptions = '';
 var logo_sizes = '';	
 var poster_sizes = '';
 var profile_sizes = '';
+
+
+$(document).ready(function() {
+	$('#movie-search-form').submit(function() {
+		event.preventDefault();
+		var thisSearch = $('#movieInput').val();
+		var searchMovie = 'https://api.themoviedb.org/3/search/movie?query='+ thisSearch +'&api_key='+apiKey;
+		$.getJSON(searchMovie, function(searchData) {
+			console.log(searchData);
+			var html = "";
+			var x = 0;
+			var searchMovieArray = searchData.results;
+
+			for(i=0; i<searchMovieArray.length; i++){
+				x++;
+				var isAdult = searchMovieArray[i].adult;
+				var backdrop_path = searchMovieArray[i].backdrop_path;
+				var genre_ids = searchMovieArray[i].genre_ids;
+				var movieId = searchMovieArray[i].id;
+				var title = searchMovieArray[i].title;
+				var overview = searchMovieArray[i].overview;
+				var popularity = searchMovieArray[i].popularity;
+				var posterPath = searchMovieArray[i].poster_path;
+				var releaseDate = searchMovieArray[i].release_date;
+				var voteAverage = searchMovieArray[i].vote_average;
+				var voteCount = searchMovieArray[i].vote_count;	
+
+				if(i==0){
+					html += '<div class="movie-row">';
+				}
+
+				if(x==5){
+					html += '</div>';
+					html += '<div class="movie-row">';
+					x=1;
+				}
+				html += '<div class="now-playing-movie">';
+				html += '<img title="'+overview+'" alt="'+title+'" src="'+basePath+'w300'+posterPath+'">';
+				html += '</div>';
+				$('#now-playing-wrapper').html(html);
+				if(i == (searchMovieArray.length-1)){
+					html += '</html>';
+					// $(html).appendTo('#now-playing-wrapper');
+				}
+			}
+			console.log(html);
+		});	
+	})
+	
+});
+
 
 var siteConfig = 'https://api.themoviedb.org/3/configuration?api_key='+apiKey;
 
